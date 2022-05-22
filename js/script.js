66a1ea534e75e78498926c1fa92924891172efea
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-22 12:51:11 +0100

    add  Almighty

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//  // Almight function (+)
+
+let a = 1;
+let b = 5;
+let c = 6; 
+
+function Almighty(){
+    return (-b + Math.sqrt( b**2-4*a*c ))/2*a
+}
+
+// Invoke function
+
+alert(Almighty());
+console.log(Almighty());
+document.write(Almighty());
